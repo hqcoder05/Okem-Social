784bfea3ec3dd87fed365d30d2d481381a2481b1
Chithanh2109 <thanhnc5187@ut.edu.vn>
2025-11-20 18:54:16 +0700

    update

---
+++ Migrations/20251120115156_AddNicknameBio.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Okem_Social.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNicknameBio : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Bio",
+                table: "Users",
+                type: "character varying(300)",
+                maxLength: 300,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Nickname",
+                table: "Users",
+                type: "character varying(40)",
+                maxLength: 40,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Bio",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Nickname",
+                table: "Users");
+        }
+    }
+}
